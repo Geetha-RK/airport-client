f88a722d4f157f6065cb9213047e3f164662c13f
Geetha R Kumar <geetha2work@gmail.com>
2025-04-12 23:17:28 -0400

    Build the airline api call and displayed on screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import './Home.scss'
 
 const Home = () => {
+  const [airline,setAirline] = useState({});
+
+  useEffect(()=>{
+      const fetchAirlines= async() => {
+        try {
+          const response = await axios.get('http://localhost:5050/api/airlines');
+          setAirline(response.data);
+          console.log(response.data);
+        } catch (error) {
+          console.log("Error:",error);
+        }
+      }
+      fetchAirlines();
+  },[])
+
   return (
     <div>
-        
+      {airline.length > 0 ? 
+        airline.map((airline,index)=>{
+          return(
+            <>
+              <p>airline Name:{airline.airline_name}</p>
+              <p>airline IATA:{airline.iata_code}</p>
+            </>
+          )
+        })
+      :
+        <p>No Airlines found</p>
+      }
+       
+
     </div>
   )
 }
